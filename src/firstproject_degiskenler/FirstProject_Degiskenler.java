48c08755378456e39ef0f855b1a2c7bdb1dd8a8a
AzadCoder <azad14961@gmail.com>
2022-10-06 14:42:12 +0300

    first commit

---
+++ src/firstproject_degiskenler/FirstProject_Degiskenler.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package firstproject_degiskenler; // Paket ismi
+
+/**
+ *
+ * @author Randy
+ */
+public class FirstProject_Degiskenler { // CLASS İSMİ
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Kodlar buraya
+        int sayi = 5;
+        double x = 2.5;
+        double k;
+        k = sayi+x;
+        System.out.println(sayi+" + "+ x +" = " + k);
+                
+        k = sayi / x;
+        System.out.println(sayi+" / "+ x +" = " + k);
+                
+                
+    }
+    
+}
